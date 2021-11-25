f04a602d73aa98ff58886d890080c14b8778e33d
iamcarminecristian <c.cruoglio2@studenti.unisa.it>
2021-11-25 15:54:24 +0100

    thirt

--- src/main/scala/ahocorasick/trie/handler/EmitHandler.java
+++
@@ -1,7 +0,0 @@
-package ahocorasick.trie.handler;
-
-import ahocorasick.trie.Emit;
-
-public interface EmitHandler {
-    boolean emit(Emit emit);
-}
--- src/main/scala/ahocorasick/trie/handler/StatefulEmitHandler.java
+++
@@ -1,9 +0,0 @@
-package ahocorasick.trie.handler;
-
-import java.util.List;
-
-import ahocorasick.trie.Emit;
-
-public interface StatefulEmitHandler extends EmitHandler {
-    List<Emit> getEmits();
-}
